0356968bab5347943d162f88e99fcf30e64b076d
supraja-maddineni <suprajamaddineni345@gmail.com>
2023-06-01 14:04:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request
+from flask_wtf import Form 
+from wtforms import StringField,SubmitField,PasswordField
+from wtforms.validators import DataRequired
+FAI=Flask(__name__)
+
+class NameForm(Form):
+    name=StringField(validators=[DataRequired()])
+    password=PasswordField(validators=[DataRequired()])
+    submit=SubmitField()
+
+
+@FAI.route('/webforms',methods=['GET','POST'])
+def webforms():
+    NFO=NameForm()
+    if request.method=='POST':
+        NFD=NameForm(request.form)
+        if NFD.validate():
+            return NFD.data 
+    return render_template('webforms.html',NFO=NFO)
+
+if __name__=='__main__':
+    FAI.run(debug=True)
